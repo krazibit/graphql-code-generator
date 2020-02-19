f5becd4d04ddd64f63b2bf1d04ba269a09cb1e26
Olamide Olatunji <olamide@smartape.io>
2020-02-19 10:44:04 +0200

    update with upstream

--- packages/plugins/typescript/graphql-apollo-server/src/index.ts
+++ packages/plugins/typescript/graphql-apollo-server/src/index.ts
@@ -5,11 +5,7 @@ import { GraphQLRequestVisitor } from './visitor';
 import { extname } from 'path';
 
 export const plugin: PluginFunction<RawClientSideBasePluginConfig> = (schema: GraphQLSchema, documents: Types.DocumentFile[], config: RawClientSideBasePluginConfig) => {
-  const allAst = concatAST(
-    documents.reduce((prev, v) => {
-      return [...prev, v.content];
-    }, [])
-  );
+  const allAst = concatAST(documents.map(v => v.document));
   const allFragments: LoadedFragment[] = [
     ...(allAst.definitions.filter(d => d.kind === Kind.FRAGMENT_DEFINITION) as FragmentDefinitionNode[]).map(fragmentDef => ({ node: fragmentDef, name: fragmentDef.name.value, onType: fragmentDef.typeCondition.name.value, isExternal: false })),
     ...(config.externalFragments || []),
@@ -25,7 +21,7 @@ export const plugin: PluginFunction<RawClientSideBasePluginConfig> = (schema: Gr
 
 export const validate: PluginValidateFn<any> = async (schema: GraphQLSchema, documents: Types.DocumentFile[], config: RawClientSideBasePluginConfig, outputFile: string) => {
   if (extname(outputFile) !== '.ts') {
-    throw new Error(`Plugin "typescript-graphql-request" requires extension to be ".ts"!`);
+    throw new Error(`Plugin "typescript-graphql-apollo-server" requires extension to be ".ts"!`);
   }
 };
 
